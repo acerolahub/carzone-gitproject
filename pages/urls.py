4b9b872c2a8372e17c41d2401a90f00994c41410
ralphsagbo <ralphsagbo2@gmail.com>
2021-04-17 10:50:05 +0200

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+"""carzone/pages URL Configuration
+phil_2: when you create a pages, you lust include an urls.py
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+]
