304dff861f84990becc2463cdc309b79e891101d
ralphsagbo <ralphsagbo2@gmail.com>
2021-04-24 16:41:47 +0200

    accounts app creation

--- carzone/settings.py
+++ carzone/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'cars.apps.CarsConfig',
     'pages.apps.PagesConfig',     # phil_1: that's come from pages/apps.py/PagesConfig
+    'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
