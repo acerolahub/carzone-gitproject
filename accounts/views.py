87282cc9cab6d5a46b2bd632a3e7c750a8c00f0a
ralphsagbo <ralphsagbo2@gmail.com>
2021-04-24 17:17:24 +0200

    registeration, login, csrf

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,11 @@ def login(request):
     return render(request, 'accounts/login.html')
 
 def register(request):
-    return register(request, 'accounts/register.html')
+    if request.method == 'POST':
+        print('this is post method')
+        return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
 
 def dashboard(request):
     return render(request, 'accounts/dashboard.html')
